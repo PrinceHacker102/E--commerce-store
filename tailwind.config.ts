1052e1fc29c4479391fa2b3a20cdc89bc2b82d32
Muhammad Anas <107877220+PrinceHacker102@users.noreply.github.com>
2024-10-13 01:25:09 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      yellow: "#fece00",
+      black: "#000",
+      white: "#fff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
